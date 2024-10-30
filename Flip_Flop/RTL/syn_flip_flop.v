9264a31f8dddf450de14825b976693f22288a059
AnsenP <ansenpan169@gmail.com>
2024-10-30 17:13:06 +0800

    Create syn_flip_flop.v

---
+++ Flip_Flop/RTL/syn_flip_flop.v
@@ -0,0 +1,20 @@
+module syn_flip_flop
+(
+input wire sys_clk , 
+input wire sys_rst_n, 
+ input wire key_in ,
+ 
+ output reg led_out 
+ );
+
+ 
+ always@(posedge sys_clk or negedge sys_rst_n) begin
+
+         if(sys_rst_n == 1'b0) begin
+          led_out <= 1'b0; 
+            end
+              else begin
+                led_out <= key_in;
+                  end
+end
+ endmodule
